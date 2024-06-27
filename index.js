de2ca98c45ddf5239b4b321f235e4d1c0921ce0c
GOKU1301 <154021053+GOKU1301@users.noreply.github.com>
2024-06-27 18:57:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 var randomnumber = Math.floor(Math.random() * 6) + 1; // Ensure a number between 1 and 6
 if(randomnumber==1)
-  document.querySelector(".dice img.img1").setAttribute("src","./images/dice1.png");
+  document.querySelector(".dice img.img1").setAttribute("src","./dice1.png");
 else if(randomnumber==2)
-  document.querySelector(".dice img.img1").setAttribute("src","./images/dice2.png");
+  document.querySelector(".dice img.img1").setAttribute("src","./dice2.png");
 else if(randomnumber==3)
-  document.querySelector(".dice img.img1").setAttribute("src","./images/dice3.png");
+  document.querySelector(".dice img.img1").setAttribute("src","./dice3.png");
 else if(randomnumber==4)
-  document.querySelector(".dice img.img1").setAttribute("src","./images/dice4.png");
+  document.querySelector(".dice img.img1").setAttribute("src","./dice4.png");
 else if(randomnumber==5)
-  document.querySelector(".dice img.img1").setAttribute("src","./images/dice5.png");
+  document.querySelector(".dice img.img1").setAttribute("src","./dice5.png");
 else if(randomnumber==6)
-  document.querySelector(".dice img.img1").setAttribute("src","./images/dice6.png");
+  document.querySelector(".dice img.img1").setAttribute("src","./dice6.png");
 
 var randomnumber2 = Math.floor(Math.random() * 6) + 1; // Ensure a number between 1 and 6
 if(randomnumber2==1)
-  document.querySelector(".dice img.img2").setAttribute("src","./images/dice1.png");
+  document.querySelector(".dice img.img2").setAttribute("src","./dice1.png");
 else if(randomnumber2==2)
-  document.querySelector(".dice img.img2").setAttribute("src","./images/dice2.png");
+  document.querySelector(".dice img.img2").setAttribute("src","./dice2.png");
 else if(randomnumber2==3)
-  document.querySelector(".dice img.img2").setAttribute("src","./images/dice3.png");
+  document.querySelector(".dice img.img2").setAttribute("src","./dice3.png");
 else if(randomnumber2==4)
-  document.querySelector(".dice img.img2").setAttribute("src","./images/dice4.png");
+  document.querySelector(".dice img.img2").setAttribute("src","./dice4.png");
 else if(randomnumber2==5)
-  document.querySelector(".dice img.img2").setAttribute("src","./images/dice5.png");
+  document.querySelector(".dice img.img2").setAttribute("src","./dice5.png");
 else if(randomnumber2==6)
-  document.querySelector(".dice img.img2").setAttribute("src","./images/dice6.png");
+  document.querySelector(".dice img.img2").setAttribute("src","./dice6.png");
 
 if(randomnumber>randomnumber2)
   document.querySelector("h1").innerHTML="Player 1 wins!!!";
